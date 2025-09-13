4a48165d1e772c2f788a296c46f471ee2c21f560
codetivated <melisa@heying.us>
2025-09-13 09:11:26 -0500

    get access to the form template element via @ViewChild

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { InputComponent } from '../../../shared/input/input.component';
 import { FormsModule } from '@angular/forms';
@@ -11,10 +11,16 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  onSubmit(ticket: { title: string; request: string }, form: HTMLFormElement) {
+  // @ViewChild('form'): Tells Angular to look for a template reference variable (e.g. #form) in the template.
+  // form?: The ? means it’s optional — it might be undefined until the view initializes.
+  // ElementRef<HTMLFormElement>: This provides direct access to the native DOM node, and the generic HTMLFormElement tells TypeScript what kind of element to expect.
+
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
+  onSubmit(ticket: { title: string; request: string }) {
     console.log('Form submitted', ticket);
     console.log('Title:', ticket.title);
     console.log('Request:', ticket.request);
-    form.reset();
+    this.form?.nativeElement.reset();
   }
 }
