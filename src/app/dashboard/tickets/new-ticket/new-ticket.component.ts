928617e08f071a9c148a7b4cccde3237a8dcfc78
codetivated <melisa@heying.us>
2025-09-13 14:47:53 -0500

    use viewChild() signal function

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, viewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { InputComponent } from '../../../shared/input/input.component';
 import { FormsModule } from '@angular/forms';
@@ -11,14 +11,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
-  // @ViewChildren is used to query and access multiple elements, directives, or components in the view DOM of a component class. It returns a QueryList
-  // @ViewChild is used to query and access a single element, directive, or component in the view DOM of a component class. It returns the first matching element.
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+
+  // signal version - Angular 16+
+  // private form = viewChild<ElementRef<HTMLFormElement>>('form');
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   onSubmit(ticket: { title: string; request: string }) {
     console.log('Form submitted', ticket);
     console.log('Title:', ticket.title);
     console.log('Request:', ticket.request);
-    this.form?.nativeElement.reset();
+
+    // this.form?.nativeElement.reset();
+
+    // signal version - Angular 16+
+    // this.form()?.nativeElement.reset();
+    this.form().nativeElement.reset();
   }
 }
+
+//! There is also a viewChildren signal version
+//! private buttons = viewChildren<ButtonComponent>('btn');
+//! on some method
+//! this.buttons().forEach((btn) => console.log(btn));
