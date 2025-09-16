8d7b73674d41662ed8d16bd66e3dacf7037f669a
codetivated <melisa@heying.us>
2025-09-16 09:20:48 -0500

    add @for to add ticket to array (not finished); introduce utility variable

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
   imports: [],
   templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  styleUrl: './ticket.component.css',
 })
-export class TicketComponent {
-
-}
+export class TicketComponent {}
